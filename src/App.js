fa459af1632edf6e8a089411e98a4a0e3fc8cef2
sehriban <sehribanmatur9@gmail.com>
2021-12-12 15:52:42 -0500

    app and index arranged

--- src/App.js
+++ src/App.js
@@ -1,18 +1,77 @@
-import { useState } from "react";
-
 import "./App.css";
-import AddTask from "./components/AddTask/AddTask";
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
-import MyHeader from "./components/Header/Button";
-function App(props) {
-  
-  
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Study React Pre-Class Notes",
+      day: "Dec 12th at 2:30pm",
+      isDone: false,
+    },
+    {
+      id: 2,
+      text: "Feed the Dog",
+      day: "Dec 13th at 1:30pm",
+      isDone: true,
+    },
+    {
+      id: 3,
+      text: "Attend In-Class",
+      day: "Dec 14th at 3:00pm",
+      isDone: false,
+    },
+  ]);
+
+  const [showAddTask, setShowAddTask] = useState(false);
+
+  // ADD TASK
+  const addTask = (newTask) => {
+    // console.log("Add Task From App.js");
+    const id = Math.floor(Math.random() * 100) + 1;
+    const addNewTask = { id, ...newTask };
+    setTasks([...tasks, addNewTask]);
+  };
+
+  // DELETE TASK
+  const deleteTask = (deletedTaskId) => {
+    // console.log("delete", deletedTaskId);
+    setTasks(tasks.filter((task) => task.id !== deletedTaskId));
+  };
+
+  //TOGGLE DONE
+  const toggleDone = (toggleDoneId) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === toggleDoneId ? { ...task, isDone: !task.isDone } : task
+      )
+    );
+  };
+
+  // SHOW ADD TASK
+  const toggleShow = () => setShowAddTask(!showAddTask);
+
+  // DELETE ALL TASKS
+  // TO DO FOR YOU
+
   return (
-    <div className='App'>
-      <h1>TASK TRACKER APP</h1>
+    <div className="container">
+      <Header
+        title="TASK TRACKER"
+        showAddTask={showAddTask}
+        toggleShow={toggleShow}
+      />
 
-      <MyHeader  />
-   <AddTask />
+      {showAddTask && <AddTask addTask={addTask} />}
+
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} deleteTask={deleteTask} toggleDone={toggleDone} />
+      ) : (
+        <p style={{ textAlign: "center" }}>NO TASK TO SHOW</p>
+      )}
     </div>
   );
 }
